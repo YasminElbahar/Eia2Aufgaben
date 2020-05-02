2dc42710c8fd9e853c41d2ea56d2af47b907cc9c
YasminElbahar <63103506+YasminElbahar@users.noreply.github.com>
2020-05-02 14:25:04 +0200

    Add files via upload

---
+++ Aufgabe_2/SequenzmemoryT.ts
@@ -0,0 +1,13 @@
+namespace Sequenzmemory {
+
+//Select a word or type input//
+    let givenSequenzes: HTMLInputElement = <HTMLInputElement>document.getElementById("givenSequenzes");
+    let input: HTMLInputElement = <HTMLInputElement>document.getElementById("input");
+    let selectSequenze: HTMLInputElement = <HTMLInputElement>document.getElementById("selectSequenze");
+    let typeInput: HTMLInputElement = <HTMLInputElement>document.getElementById("typeInput");
+
+    //Timing//
+    let setTime: HTMLButtonElement = <HTMLButtonElement>document.getElementById("setTime");
+    let startDuration: HTMLButtonElement = <HTMLButtonElement>document.getElementById("startDuration");
+    let word: HTMLDivElement = <HTMLDivElement>document.getElementById("word"); 
+}
